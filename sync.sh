3a9e6e9a59a10783f1db880273c13a37601972bf
Dean Todevski <dean.todevski@gmail.com>
2016-12-06 15:43:57 +0100

    Fixed permissions and path

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ORIG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/orig"
+ORIG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DEST=/var/www/dean/
 
-find $ORIG -type f \( -iname \*.html -o -iname \*.css \) -exec cp {} $DEST \;
+sudo find $ORIG -type f \( -iname \*.html -o -iname \*.css \) -exec cp {} $DEST \;
